ae34840a5f747fc4b69dc443b82642bab8dd2a66
R-TURPIN <159109168+R-TURPIN@users.noreply.github.com>
2025-12-16 19:02:26 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // Si tu as un fichier css, sinon retire cette ligne
+
+// J'ai supprimé la ligne "import './index.css'" ici car le fichier n'existe pas
+// et que Tailwind est chargé via le fichier index.html
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
