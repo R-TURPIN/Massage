fbb3a163ded5924166e9796167ba1ebdcbc14b5d
R-TURPIN <159109168+R-TURPIN@users.noreply.github.com>
2025-12-17 21:03:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom' // On utilise HashRouter ici
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Dashboard from './Dashboard'
 import './index.css'
@@ -8,13 +8,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        {/* Route par défaut (Le site public) */}
-        <Route path="/" element={<App />} />
-        
-        {/* Route secrète (L'outil de ton pote) */}
-        <Route path="/app" element={<Dashboard />} />
-      </Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/app" element={<Dashboard />} />
     </HashRouter>
   </React.StrictMode>,
 )
