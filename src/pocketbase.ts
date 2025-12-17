d587eb4c01efd189511cffb7ab338c18046cdfc4
R-TURPIN <159109168+R-TURPIN@users.noreply.github.com>
2025-12-17 18:46:38 +0100

    Update pocketbase.ts

--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
 
 // Ton URL PocketBase (je l'ai récupérée de ton message précédent)
-export const pb = new PocketBase('http://pocketbase-fgk0so8swgs488kwkc4kkoos.37.27.191.8.sslip.io');
+export const pb = new PocketBase('https://pocketbase-fgk0so8swgs488kwkc4kkoos.37.27.191.8.sslip.io');
